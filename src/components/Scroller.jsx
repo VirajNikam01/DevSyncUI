07b8afc49d7f98ff52a592ff34ebbd9c2726d7bc
virajnikam01 <virajnikam01@gmail.com>
2024-10-20 15:13:57 +0530

    added scroll buttons fun

--- src/components/Scroller.jsx
+++ src/components/Scroller.jsx
@@ -3,19 +3,40 @@ import Card from "./Card";
 import { Card_Data } from "../utils/static";
 
 const Scroller = () => {
+  const scrollerRef = useRef();
+
+  const shownNextCards = () => {
+    const cardWidth = parseInt(
+      getComputedStyle(scrollerRef.current.children[0]).width
+    );
+    scrollerRef.current.scrollLeft += cardWidth * 2;
+  };
+
+  const showPrevCards = () => {
+    const cardWidth = parseInt(
+      getComputedStyle(scrollerRef.current.children[0]).width
+    );
+    scrollerRef.current.scrollLeft -= cardWidth * 2;
+  };
+
   return (
     <div className="scrollerWrapper flex items-center sm:gap-4 justify-center ">
-      <button className=" p-2 sm:p-4 border">{"<"}</button>
-      <div className="scroller w-full  flex gap-2 p-2 overflow-x-scroll">
-        <div className="flex gap-3">
-          {Card_Data.map((data, index) => (
-            <div key={index}>
-              <Card key={data.id} data={data} />
-            </div>
-          ))}
-        </div>
+      <button onClick={showPrevCards} className=" p-2 sm:p-4 border">
+        {"<"}
+      </button>
+      <div
+        ref={scrollerRef}
+        className="scroller w-full  flex gap-2 p-2 overflow-x-scroll scroll-smooth"
+      >
+        {Card_Data.map((data, index) => (
+          <div key={index} className="w-fit">
+            <Card key={data.id} data={data} />
+          </div>
+        ))}
       </div>
-      <button className="p-2 sm:p-4 border">{">"}</button>
+      <button onClick={shownNextCards} className="p-2 sm:p-4 border">
+        {">"}
+      </button>
     </div>
   );
 };
